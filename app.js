4e2fd8c29a235f4ed4cf0f04b34228b791c164ee
Andrea Anaya <andrayantelo@gmail.com>
2015-02-10 14:01:09 -0600

    made a List object

---
+++ app.js
@@ -0,0 +1,18 @@
+#todo list webapp
+
+var List = function () {
+    
+    this.listItems = [];
+
+    this.addToList = function(item) {
+        if (listItems.length === 0) {
+            listItems[0] = item;
+        } else {
+            listItems[listItems.length - 1] = item;
+            
+        }
+        return listItems;
+    };
+        
+    
+    
