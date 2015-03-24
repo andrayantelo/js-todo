886881ed6dea86ac7c0bea16f472363d2bf0a9e8
Andrea Anaya <andrayantelo@gmail.com>
2015-03-24 15:45:50 -0500

    commented out some stuff, added storage to addToList

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ $(document).ready(function() {
         todoList.addToList(toAdd);
         todoList.generateListDiv($('#list'));
         $('#checkListEntry').val('');
-        todoList.storeList();
     })
     
     
@@ -37,16 +36,16 @@ $(document).ready(function() {
     
     $('#list').sortable();
     
-    if (typeof(Storage) !="undefined") {
+//    if (typeof(Storage) !="undefined") {
       // store
-        todoList.storeList();
+//        todoList.storeList();
       //retrieve
-        todoList.retrieveList();
-    }
+//        todoList.retrieveList();
+//    }
     
-    else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
-     }
+//    else {
+//    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+//     }
     
     
 });
@@ -60,8 +59,12 @@ var List = function () {
     self.addToList = function(item) {
         if (self.listItems.length === 0) {
             self.listItems[0] = item;
+            var todos = $(".list").html();
+            localStorage.setItem('todos', todos);
         } else {
             self.listItems[self.listItems.length] = item;
+            var todos = $(".list").html();
+            localStorage.setItem('todos', todos);
             
         }
         return this.listItems;
@@ -82,14 +85,14 @@ var List = function () {
         
     };
     
-    self.storeList = function() {
-            var myList = JSON.stringify(self.listItems);
-            localStorage.setItem("myList", myList);
-        };
+   // self.storeList = function() {
+   //         var myList = JSON.stringify(self.listItems);
+   //         localStorage.setItem("myList", myList);
+   //     };
         
-    self.retrieveList = function() {
-        document.getElementById("list").innerHTML = localStorage.getItem("myList");
-        };
+   // self.retrieveList = function() {
+   //     document.getElementById("list").innerHTML = localStorage.getItem("myList");
+   //     };
 
 };
         
