a076265b853ce7c8e0eec5e28dc5c7fea65161c7
Andrea Anaya <andrayantelo@gmail.com>
2015-07-07 13:35:38 -0500

    adding comments to js file to remember how this app works so that I can fix it

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-
+// if comment is indented it means it is lines of code that have been
+//commented out. Otherwise they are just comments.
 
 $(document).ready(function() {
     
     
     
     $(function() {
+//the textbox has the typing cursor in it ready to type
       $("#checkListEntry").focus();
     })
     
     $('#button').mouseenter(function() {
+//changes css for the Add button when mouse hovers over it
         $('#button').css('background-color','white');
         $('#button').css('color', '#3d1256');
         $('#button').css('border', 'solid');
         $('#button').css('borderWidth', '1px');
     })
     $('#button').mouseleave(function() {
+// changes button back to normal css when mouse leaves
         $('#button').css('background-color','#3d1256');
         $('#button').css('color', 'white');
         $('#button').css('border', 'none');
     })
     
     $('#button').click(function(){
+//when button is clicked
         var toAdd = $('#checkListEntry').val();
+//get the value of #checkListEntry, presumably someone typed
+//something to be added before clicking
         todoList.addToList(toAdd);
+// add this value to 
         todoList.generateListDiv($('#list'));
         $('#checkListEntry').val('');
     })
@@ -66,9 +74,11 @@ var List = function (localStorageKey) {
     self.listItems = [];
 
     self.addToList = function(item) {
+// if the list is empty then the first element will be 'item'
         if (self.listItems.length === 0) {
             self.listItems[0] = item;
-            
+// if the list is not empty than item will be the nth item in
+// the list, where n is equal to the length of listItems    
         } else {
             self.listItems[self.listItems.length] = item;
             
@@ -100,7 +110,7 @@ var List = function (localStorageKey) {
         };
         
     self.retrieveList = function() {
-        // Returns true if the list successfully loaded, otherwise false
+// Returns true if the list successfully loaded, otherwise false
         var item = localStorage.getItem(self.localStorageKey);
         if (item === undefined) {
             return false;
@@ -112,5 +122,12 @@ var List = function (localStorageKey) {
 };
         
     
-
+// make a new List object called todoList with self.localStorageKey set
+// equal to 'todoList'
 var todoList = new List('todoList');
+
+//new object todoList has the following properties:
+// self.localStorageKey = todoList
+// self.listItems = []
+// and the following methods: addTolist, generateListDiv, removeFromList,
+// storeList, retrieveList
