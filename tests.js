d5a870561ee3a149c6b89d5747e3e09d53d695dc
Andrea Anaya <andrayantelo@gmail.com>
2015-07-10 21:11:59 -0500

    first successful test

--- tests.js
+++ tests.js
@@ -2,7 +2,8 @@
 
 
 test("addToList test", function() {
+    var item = 'item';
     var myList = new List('myList');
-    myList.addToList('item');
-    equal(myList[0], 'item');
+    myList.addToList(item);
+    equal(myList.listItems[0], item);
 });
