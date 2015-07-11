205c5a589bdf9e4de26165fe8a8bda35b5086996
Andrea Anaya <andrayantelo@gmail.com>
2015-07-10 21:23:26 -0500

    fixed for lists

--- tests.js
+++ tests.js
@@ -6,4 +6,13 @@ test("addToList test", function() {
     var myList = new List('myList');
     myList.addToList(item);
     equal(myList.listItems[0], item);
+    
+    var car = 'car';
+    myList.addToList(car);
+    equal(myList.listItems[1], car);
+    
+ /* don't think you can do this for lists */
+    var itemList = ['item', 'car'];
+    deepEqual(myList.listItems, ['item', 'car']);
+    
 });
