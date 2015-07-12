8b8aeb5238834e9cdc61a52b1013ac13114b170d
Andrea Anaya <andrayantelo@gmail.com>
2015-07-12 16:55:47 -0500

    making sure tests still "work" properly

--- tests.js
+++ tests.js
@@ -10,14 +10,15 @@ test("addToList test", function() {
     expect(4);
     
     myList.addToList(item);
+    console.log(myList.listItems);
     equal(myList.listItems[0], item);
     
     myList.addToList(car);
     equal(myList.listItems[1], car);
 
     deepEqual(myList.listItems, ['item', 'car']);
-    
-    equal(myList.addToList(book), false);
+    myList.addToList(book)
+    equal(myList.isOk, true);
     
 });
 
@@ -28,8 +29,8 @@ test("removeFromList test", function() {
     myList.removeFromList('item');
     
     deepEqual(myList.listItems, ['car', 'book']);
-    
-    equal(myList.removeFromList('dog'), false);
+    myList.removeFromList('dog');
+    equal(myList.isOk, false);
 });
 
 
