fe60a43674ed835a683548a1f450af240cc79ce7
Andrea Anaya <andrayantelo@gmail.com>
2015-07-13 12:12:07 -0500

    cleaned up comment lines

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-/* if comment is indented it means it is lines of code that have been
-commented out. Otherwise they are just comments.*/
 
 $(document).ready(function() {
     
     
     
     $(function() {
-//the textbox has the typing cursor in it ready to type
+    //the textbox has the typing cursor in it ready to type
+    
       $("#checkListEntry").focus();
     });
     
     $('.button').mouseenter(function() {
-//changes css for the Add button when mouse hovers over it
+    //changes css for the Add button when mouse hovers over it
+    
         $(this).css('background-color','white');
         $(this).css('color', '#3d1256');
         $(this).css('border', 'solid');
@@ -19,7 +19,8 @@ $(document).ready(function() {
         
     });
     $('.button').mouseleave(function() {
-// changes button back to normal css when mouse leaves
+    // changes button back to normal css when mouse leaves
+    
         $(this).css('background-color','#3d1256');
         $(this).css('color', 'white');
         $(this).css('border', 'none');
@@ -27,17 +28,23 @@ $(document).ready(function() {
     });
     
     $('#addButton').click(function(){
-//when button is clicked
+    //when button is clicked
+    
         var toAdd = $('#checkListEntry').val();
-/*get the value of #checkListEntry, presumably someone typed
-something to be added before clicking */
+        
+    /*get the value of #checkListEntry, presumably someone typed
+    something to be added before clicking */
+    
         todoList.addToList(toAdd);
-// add this value to 
+        
+    // add this value to 
+    
         todoList.generateListDiv($('#list'));
         $('#input').find("form")[0].reset();    //empties input area
     });
 
-//stores list when save button is clicked. has an alert for now.    
+    //stores list when save button is clicked. has an alert for now.  
+      
     $('#saveButton').click(function(){
         todoList.storeList();
         alert("List saved");
@@ -57,12 +64,13 @@ something to be added before clicking */
     
     
     $(document).on('click', '.item', function() {
+        
        // var toRemove = $('#list').index(this);
        // console.log(toRemove);
+       
         todoList.removeFromList(this.innerHTML);
         todoList.generateListDiv($('#list'));
       
-        
     });
     
     $('#list').sortable();
@@ -96,11 +104,14 @@ var List = function (localStorageKey) {
     self.isOk = true;
 
     self.addToList = function(item) {
-// if the list is empty then the first element will be 'item'
+    // if the list is empty then the first element will be 'item'
+    
         if (self.listItems.length === 0) {
             self.listItems[0] = item;
-/* if the list is not empty than item will be the nth item in
-the list, where n is equal to the length of listItems   */
+            
+    /* if the list is not empty than item will be the nth item in
+    the list, where n is equal to the length of listItems   */
+    
         } else {
             self.listItems[self.listItems.length] = item;
             
@@ -110,22 +121,28 @@ the list, where n is equal to the length of listItems   */
     };
     
     self.generateListDiv = function(listDiv) {
-// empty the child nodes and content from the element 'listDiv' 
+    // empty the child nodes and content from the element 'listDiv' 
+    
         listDiv.empty();
-//for each array element in listItems
+    //for each array element in listItems
+    
         self.listItems.forEach( function(toAdd) {
-/*add to the beginning of the element listDiv the element li with class
-'item' and with the value toAdd */
+    /*add to the beginning of the element listDiv the element li with class
+    'item' and with the value toAdd */
+    
             listDiv.prepend('<li class="item">' + toAdd + '</li>');
         });
     };
     
     self.removeFromList = function(item) {
-//get the index of the item
+    //get the index of the item
+    
         var indexOfItem = self.listItems.indexOf(item);
-// if the item is in the list
+    // if the item is in the list
+    
         if (indexOfItem != -1) {
-// start at position indexofItem and remove 1 element of the list 
+    // start at position indexofItem and remove 1 element of the list 
+    
             self.listItems.splice(indexOfItem, 1);
             self.storeList();
             return self;
@@ -146,31 +163,32 @@ the list, where n is equal to the length of listItems   */
     };
     
     self.storeList = function() {
-// convert a javascript value (self.listItems) to a JSON string
+    // convert a javascript value (self.listItems) to a JSON string
+    
             var myList = JSON.stringify(self.listItems);
-/* access the current domain's local Storage object and add a data item
-(myList) to it */
+    /* access the current domain's local Storage object and add a data item
+    (myList) to it */
+    
             localStorage.setItem(self.localStorageKey, myList);
             return self;
     };
     
     
-// put a flag on the list instead of false false true
-    
-    
     self.retrieveList = function() {
-// Returns true if the list successfully loaded, otherwise false
+    // Returns true if the list successfully loaded, otherwise false
+    
         var item = localStorage.getItem(self.localStorageKey);
         if (item === undefined) {
             self.isOk = false;
             return self;
         }
-// to account for when storage is empy
+    // to account for when storage is empy
+    
         else if (item === null) {
             self.isOk = false;
             return self;
         }
-// 
+ 
         self.listItems = JSON.parse(item);
         
         return self;
--- tests.js
+++ tests.js
@@ -34,3 +34,5 @@ test("removeFromList test", function() {
 });
 
 
+
+
